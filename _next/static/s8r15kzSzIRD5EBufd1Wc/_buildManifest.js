77dfb8ed80e23f517dd99ca37cc64ae28063b604
Flavio Leonardo Machado <60984765+LeonardoMachado30@users.noreply.github.com>
2023-01-23 14:12:23 -0300

    Updates

---
+++ _next/static/s8r15kzSzIRD5EBufd1Wc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-a942f2884c03cf3c.js","static/chunks/pages/index-d82efb2e3824a98b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
