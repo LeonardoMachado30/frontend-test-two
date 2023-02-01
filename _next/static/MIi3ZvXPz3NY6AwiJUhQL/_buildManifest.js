4babf9998367aa58405037b16224a2a88dbcf836
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-02-01 12:57:12 -0300

    Updates

---
+++ _next/static/MIi3ZvXPz3NY6AwiJUhQL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-6288eb49a77b297d.js","static/chunks/pages/index-4e6283a509276d57.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/N1gXR3xfNMDeCyBERGVFS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/654-6288eb49a77b297d.js","static/chunks/pages/index-0597fcf2b8e10f3f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
